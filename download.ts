8b00567352631be3f5eae6a89eb0f2091700562f
Matt Broussard <matt@mattb.io>
2021-02-20 22:38:07 -0800

    message downloading and json/html file output in download.ts

--- download.ts
+++ download.ts
@@ -1,8 +1,11 @@
 import _ from "lodash";
 import { OAuth2Client, authorizeAsync } from "./google_auth";
 import inquirer from "inquirer";
-import { ALL_IMPORTERS, Importer } from "./importers/importer";
+import { ALL_IMPORTERS, Importer, ImporterMessage } from "./importers/importer";
 import { google, gmail_v1 } from "googleapis";
+import mkdirp from "mkdirp";
+import { promises as fs } from "fs";
+import path from "path";
 
 inquirer.registerPrompt("datetime", require("inquirer-datepicker-prompt"));
 
@@ -21,6 +24,14 @@ interface Config {
   startDate: Date;
   endDate: Date;
   artifactTypes: ("json" | "html" | "pdf")[];
+}
+
+function b64decode(
+  encoded: string | null | undefined
+): string | null | undefined {
+  return encoded != null
+    ? Buffer.from(encoded, "base64").toString("utf8")
+    : encoded;
 }
 
 async function askForConfig() {
@@ -64,6 +75,27 @@ async function askForConfig() {
   return configAnswers;
 }
 
+function buildImporterMessage(
+  message: gmail_v1.Schema$Message
+): ImporterMessage {
+  const parts = message.payload!.parts;
+
+  const plainPart = _.find(parts, (part) => part.mimeType === "text/plain");
+  const text = b64decode(plainPart?.body?.data!) || undefined;
+
+  const htmlPart = _.find(parts, (part) => part.mimeType === "text/html");
+  const html = b64decode(htmlPart?.body?.data!)!;
+
+  const headers = message.payload!.headers;
+  const subject =
+    _.find(headers, (header) => header.name === "Subject")?.value ||
+    "(no subject)";
+
+  const date = new Date(Number(message.internalDate));
+
+  return { rawMessage: message, text, html, subject, date };
+}
+
 async function runImporter(
   importer: Importer,
   config: Config,
@@ -79,16 +111,69 @@ async function runImporter(
     userId: "me",
     ...importer.params,
     q: `${importer.params.q} ${dateQuery}`,
+
+    // TODO: paging
     maxResults: 100,
   };
   console.log("Gmail API message list params:", params);
 
+  // List messages
   const gmail = google.gmail({ version: "v1", auth });
-  const messagesResp = await gmail.users.messages.list(params);
-  const messages = messagesResp.data.messages;
+  const messageListResp = await gmail.users.messages.list(params);
+  const messageIds = _.map(messageListResp.data.messages || [], "id");
+  console.log("Listed", messageIds.length, "messages.");
 
-  // TODO: implement rest
-  console.log(messages);
+  // Download each message
+  const messageRespPromises = messageIds.map(async (id) => {
+    const resp = await gmail.users.messages.get({ id: id!, userId: "me" });
+    console.log("Downloaded message with id", id);
+    return resp;
+  });
+  const messageResps = await Promise.all(messageRespPromises);
+  const messages = _.map(messageResps, "data");
+
+  // Process each message for metadata extraction
+  for (const message of messages) {
+    await runImporterOnMessage(message, importer, config);
+  }
+
+  console.log("Done running importer", importer.name);
+}
+
+async function runImporterOnMessage(
+  message: gmail_v1.Schema$Message,
+  importer: Importer,
+  config: Config
+) {
+  const importerMessage = buildImporterMessage(message);
+  const metadata = importer.extractMetadataFromMessage(importerMessage);
+  if (!metadata) {
+    console.log(
+      "Skipping message",
+      message.id,
+      "because importer returned null"
+    );
+    return;
+  }
+
+  console.log("Imported", metadata.filename, "from message", message.id);
+
+  if (config.artifactTypes.includes("json")) {
+    const json = {
+      metadata,
+      messageId: message.id,
+      importerMessageObj: _.omit(importerMessage, "rawMessage"),
+      gmailApiMessageObj: message,
+    };
+
+    const fname = path.join(config.outDir, metadata.filename + ".json");
+    await fs.writeFile(fname, JSON.stringify(json, undefined, 2));
+  }
+
+  if (config.artifactTypes.includes("html")) {
+    const fname = path.join(config.outDir, metadata.filename + ".html");
+    await fs.writeFile(fname, importerMessage.html);
+  }
 }
 
 async function main() {
@@ -100,6 +185,8 @@ async function main() {
   const importersToRun = ALL_IMPORTERS.filter((importer) =>
     _.includes(config.importers, importer.name)
   );
+
+  await mkdirp(config.outDir);
 
   // TODO: paralellize?
   for (const importer of importersToRun) {
